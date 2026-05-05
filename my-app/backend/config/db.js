a20d9c12af724f905fac8ddd33ffaaf8fdf8fabd
VDgit03 <dangvietdung2903@gmail.com>
2026-05-05 23:03:04 +0700

    kết nối csdl

---
+++ my-app/backend/config/db.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+console.log("MySQL pool ready");
+
+export default pool;
